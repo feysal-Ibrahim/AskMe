c0959e4830921b303c7fffd3390b8b8bd0ee8e84
first-name second-name <iamfeysal@gmail.com>
2018-05-10 09:22:09 +0300

    added more element

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,44 @@
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-      center: new google.maps.LatLng(51.5, -0.2),
-      zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+
+//business logic
+function Contact(first,last,email){
+  this.firstName=first;
+  this.lastName=last;
+  this.email=email;
 }
+
+Contact.prototype.fullName =function (){
+  return this.firstName + "" + this.lastName + this.email;
+}
+
+//user interface
+$(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+
+
+
+
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+      var inputtedemail=$("input#new-email").val();
+
+      var newContact = new Contact(inputtedFirstName, inputtedLastName,inputtedemail);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input#email").val("");
+
+      $(".contact").last().click(function() {
+     $("#show-contact").show();
+     $("#show-contact h2").text(newContact.firstName);
+     $(".first-name").text(newContact.firstName);
+     $(".last-name").text(newContact.lastName);
+      $(".email").text(newContact.email);
+
+   });
+
+
+    });
+  });
